74ea2e31ef8e7cb0f2a2740b0f3e29e489175c71
Kang-Kyu Lee <kangkyu1111@gmail.com>
2017-04-26 11:19:33 -0700

    add first component

---
+++ app/assets/javascripts/components/app.js.jsx
@@ -0,0 +1,11 @@
+class App extends React.Component {
+  render() {
+    return (
+      <h2 className="header center">
+        Quick Reads
+      </h2>
+    )
+  }
+}
+
+ReactDOM.render(<App/>, document.getElementById('root'));
